3c73a468d8bda24bcd491b5fb4f1b4d652907c1e
lgallagher <lewisjgallagher@gmail.com>
2022-09-19 00:16:37 +0100

    Add radio buttons for rsvp

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, IntegerField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, IntegerField, TelField, RadioField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 
 class RSVPForm(FlaskForm):
+    attending = RadioField(label = "Will you be attending?", choices = ["Yes, I will be attending", "No, I will not be attending"])
     name = StringField(label = 'Name', validators = [DataRequired()])
     email = StringField(label = 'Contact email', validators = (DataRequired(), Email()))
-    phone = IntegerField(label = 'Contact phone number', validators = [DataRequired()])
+    phone = TelField(label = 'Contact phone number', validators = [DataRequired()])
     message = TextAreaField(label = 'Message', validators = [Length(max = 150)])
     submit = SubmitField(label = 'Submit')
     
--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,11 @@ from flask import flash, redirect, render_template, url_for, request
 def index():
     return render_template('index.html', title='Home')
 
+
 @app.route('/rsvp', methods=['GET', 'POST'])
 def rsvp():
     form = RSVPForm()
+    if form.validate_on_submit():
+        redirect(url_for('index'))
+
     return render_template('rsvp.html', title = "RSVP", form = form)
