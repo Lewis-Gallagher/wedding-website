d62dd7b1382c4f9d32066a94a39395513c80fe54
lgallagher <lewisjgallagher@gmail.com>
2022-12-26 22:14:00 +0000

    :sparkles: Change diet_req to string for email readability

--- app/routes.py
+++ app/routes.py
@@ -5,19 +5,16 @@ from app.models import Guest
 from app.email import send_email
 from flask import flash, redirect, render_template, url_for
 
-
-class BigBangException(Exception):
-    pass
-
 @app.route('/')
 @app.route('/index')
 def index():
-    raise BigBangException("BANG!")
     return render_template('index.html', title='Home')
 
 @app.route('/rsvp', methods=['GET', 'POST'])
 def rsvp():
+
     form = RSVPForm()
+
     if form.validate_on_submit():
         guest = Guest(
             attending = form.attending.data,
@@ -28,23 +25,36 @@ def rsvp():
             message = form.message.data
         )
 
-        # If guest has diet_req, turn the list into a concatenated string e.g. [1,3,5] into '135' otherwise None. SqlAlchemy doesn't allow list types.
+
+        # Cast the chosen diet requirements into a string e.g. [1,3,4] -> '134'
+        # also create an readable string for the email confirmation. e.g "vegetarian, nut allergies, fish allergies" in place of numbers.
+        diet_dict = {
+            1: 'Vegetarian',
+            2: 'Vegan',
+            3: 'Nuts allergies',
+            4: 'Fish and or shellfish allergies',
+            5: 'Other'
+        }
+
         if guest.diet_req:
             guest.diet_req = ''.join(map(str, guest.diet_req))
+            diet_string = ', '.join([diet_dict[int(i)] for i in ' '.join(guest.diet_req).split()])
         else:
-            guest.diet_req = None
+            guest.diet_req = 0
+            diet_string = 'None'
 
         # Send confirmation email of a sucessfull RSVP
         try:
             send_email(
-                subject="LG NP Wedding RSVP confirmation",
+                subject="NP LG Wedding RSVP confirmation",
                 to_emails = guest.email,
-                text_body = render_template('email/rsvp_response.txt', guest = guest),
-                html_body = render_template('email/rsvp_response.html', guest = guest)
+                text_body = render_template('email/rsvp_response.txt', guest = guest, diet_string = diet_string),
+                html_body = render_template('email/rsvp_response.html', guest = guest, diet_string = diet_string)
             )
         except Exception as err:
             flash(f'There was a problem submitting your RSVP. Please try again. If the problem persists please contact {app.config["ADMINS"][0]} - {err}', 'error')
             db.session.rollback()
+
         else:
             db.session.add(guest)
             db.session.commit()
@@ -66,6 +76,3 @@ def event_info():
 @app.route('/faq')
 def faq():
     return render_template('faq.html', title = 'FAQ')
-
-
-
