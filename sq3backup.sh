80c7d1b2892d7b3057f6f1f71296fbfb4e45ae42
lgallagher <lewisjgallagher@gmail.com>
2023-01-07 12:06:45 +0000

    :sparkles: Make bak/ file if doesn't already exist

--- sq3backup.sh
+++ sq3backup.sh
@@ -2,12 +2,15 @@
 
 set -e
 
-FILE=`pwd`/app.db
+FILE=${PWD}/app.db
 if [ -f "$FILE" ]; then
+    if [!-d ${PWD}/bak]; then
+        mkdir -p ${PWD}/bak;
+    fi;
     t=$(date "+%Y%m%d-%H%M%S");
-    echo "Backing up app.db to bak/$t.app.db.sq3";
+    echo "Backing up app.db to bak/${t}.app.db.sq3";
 
-    sqlite3 app.db ".backup 'bak/$t.app.db.sq3'";
+    sqlite3 app.db ".backup 'bak/${t}.app.db.sq3'";
 
 else 
     echo "$FILE does not exist."
