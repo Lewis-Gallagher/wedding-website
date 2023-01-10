e2cd4553b180710b2ed70637444159ae09a65e1d
Lewis Gallagher <lewisjgallagher@gmail.com>
2023-01-10 14:39:18 +0000

    ✨ Add dotenv loading

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 import os
 import secrets
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_urlsafe()
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, 'db/app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ADMINS = ['lewis@nplgwedding.com']
     
