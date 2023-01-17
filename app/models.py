fcaff55ee0ed88f9fb0aebbc438f46692fe7559c
lgallagher <lewisjgallagher@gmail.com>
2023-01-17 00:00:17 +0000

    :recycle: Name is no longer unique

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 class Guest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     attending = db.Column(db.Integer, index=False, unique=False)
-    name = db.Column(db.String(64), index=True, unique=True)
+    name = db.Column(db.String(64), index=True, unique=False)
     email = db.Column(db.String(120), index=True, unique=True)
     phone = db.Column(db.String(20), index=True, unique=False)
     diet_req = db.Column(db.String(), index=False, unique=False)
