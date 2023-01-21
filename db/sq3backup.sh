b8b2b2448f8bfb24b65e1f8faa852f2f2fad3780
lgallagher <lewisjgallagher@gmail.com>
2023-01-21 14:40:28 +0000

    :truck: Move sq3backup script to db directory

--- db/sq3backup.sh
+++ db/sq3backup.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-set -e
-
+mkdir -p ${PWD}/bak;
 FILE=${PWD}/app.db
+
 if [ -f "$FILE" ]; then
-    if [!-d ${PWD}/bak]; then
-        mkdir -p ${PWD}/bak;
-    fi;
     t=$(date "+%Y%m%d-%H%M%S");
+
     echo "Backing up app.db to bak/${t}.app.db.sq3";
 
     sqlite3 app.db ".backup 'bak/${t}.app.db.sq3'";
 
-else 
+else
     echo "$FILE does not exist."
 fi
